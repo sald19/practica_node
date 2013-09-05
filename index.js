5dc36da6810fe9ceb969b4eb6ad4d9fb6af5e46f
carlos saldana <carlos@char.carlos.com>
2013-09-05 00:41:31 -0600

    se agrego el handle y se modifico el server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var server = require("./server");
-server.iniciar();
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.iniciar;
+handle["/iniciar"] = requestHandlers.iniciar;
+handle["/subir"] = requestHandlers.subir;
+
+server.iniciar(router.route, handle);
---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function iniciar () {
+	console.log("Manipulador de peticion 'iniciar' ha sido llamado.");
+}
+
+function subir () {
+	console.log("Manipulador de peticion 'subir' ha sido llamado.");
+}
+
+exports.iniciar = iniciar;
+exports.subir = subir;
