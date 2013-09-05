93c64c208d12b7f2c2906f275d57f3c08f37f5ef
carlos saldana <carlos@char.carlos.com>
2013-09-05 00:02:41 -0600

    primer cambio

---
+++ router.js
@@ -0,0 +1,9 @@
+/**
+ * [route description]
+ * @param  {[type]} pathname
+ * @return {[type]}
+ */
+function route (pathname) {
+	console.log("A punto de rutear una peticion para " + pathname);
+}
+exports.route = route;
