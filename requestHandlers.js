75b4c7d910073029d247afa7ebe49108d4909a9c
carlos saldana <carlos@char.carlos.com>
2013-09-05 23:10:11 -0600

    cambios nuevos

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,19 @@
 function iniciar () {
 	console.log("Manipulador de peticion 'iniciar' ha sido llamado.");
+	
+	function sleep (milliSeconds) {
+		// obtener la hora actual
+		var startTime = new Date().getTime();
+		// atasca la cpu
+		while (new Date().getTime() < startTime + milliSeconds);
+	}
+	sleep(10000);
+	return "Hola iniciar";
 }
 
 function subir () {
 	console.log("Manipulador de peticion 'subir' ha sido llamado.");
+	return "Hola subir";
 }
 
 exports.iniciar = iniciar;
--- router.js
+++ router.js
@@ -3,7 +3,13 @@
  * @param  {[type]} pathname
  * @return {[type]}
  */
-function route (pathname) {
+function route (handle, pathname) {
 	console.log("A punto de rutear una peticion para " + pathname);
+	if (typeof handle[pathname] == 'function') {
+		return handle[pathname]();
+	} else {
+		console.log("No se encontro manipulador para " + pathname);
+		return "404 No Encontrado";
+	}
 }
 exports.route = route;
